646255f1f3001569a80c403569a6aab2fb07654e
Jeff Meese <jeffrey.meese@us.kbr.com>
2026-05-13 10:37:53 -0400

    Added extremely crude engine

--- src/engine.cpp
+++ src/engine.cpp
@@ -2,5 +2,16 @@
 
 namespace jmchess
 {
-  
+
+Engine::Engine()
+  : mSearch(new Search)
+{
 }
+
+Move Engine::getMove(
+  Board * board) const
+{
+  return mSearch->execute(board, board->getBoardState().sideToMove, mSearchDepth);
+}
+
+} // namespace jmchess
--- src/engine.h
+++ src/engine.h
@@ -1,11 +1,24 @@
 #pragma once
 
+#include <memory>
+
+#include "board.h"
+#include "search.h"
+
 namespace jmchess
 {
 
 class Engine
 {
+public:
+  Engine();
 
+  Move getMove(
+    Board * board) const;
+
+private:
+  std::unique_ptr<Search> mSearch;
+  std::int32_t mSearchDepth = 3;
 };
 
 }
