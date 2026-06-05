05f1618d879cf4c6a7a2126007466919adfd5732
Jeff Meese <jeffmeese@gmail.com>
2026-06-05 19:58:36 -0400

    Add make and unmake to GuiGame

--- tools/gui/application/gui_game.cpp
+++ tools/gui/application/gui_game.cpp
@@ -1,2 +1,27 @@
 #include "gui_game.h"
 
+GuiGame::GuiGame(QObject * parent)
+  : QObject(parent)
+{
+}
+
+void GuiGame::makeMove(const jmchess::Move & move)
+{
+  Game::makeMove(move);
+  emit moveMade(move);
+
+  if (isCheckmate())
+  {
+    emit checkmate();
+  }
+  else if (isStalemate())
+  {
+    emit stalemate();
+  }
+}
+
+void GuiGame::unmakeMove()
+{
+  Game::undoLastMove();
+  //emit moveUndone(move);
+}
--- tools/gui/application/gui_game.h
+++ tools/gui/application/gui_game.h
@@ -1,9 +1,25 @@
 #pragma once
 
+#include <QObject>
+
 #include "game.h"
 
-class GuiGame : public jmchess::Game
+class GuiGame : public QObject, public jmchess::Game
 {
+  Q_OBJECT
+
 public:
-  
+  explicit GuiGame(QObject * parent = nullptr);
+
+  void makeMove(const jmchess::Move & move);
+  void unmakeMove();
+
+signals:
+  void checkmate();
+  void moveMade(const jmchess::Move & move);
+  void moveUndone(const jmchess::Move & move);
+  void stalemate();
+
+private:
+
 };
